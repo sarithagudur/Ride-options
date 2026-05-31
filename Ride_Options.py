9f7d8793414b6d68a509ffab1623a18a377da07a
sarithagudur <42064481+sarithagudur@users.noreply.github.com>
2026-05-31 10:50:27 +0100

    Create Ride_Options.py

---
+++ Ride_Options.py
@@ -0,0 +1,8 @@
+user=(input("Choose your ride Car or Bike: "))
+if user=="Car":
+    user1=(input("Choose your colour Pink or Blue:"))
+    print("Your chose",user1,"Car")
+
+elif user=="Bike":
+    user2=int(input("Choose how many wheels 2 or 4:"))
+    print("Your chose",user2," wheel Bike")
